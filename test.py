329474022029443ed2682489bef4cdaedafceae2
Nikita <nnigmat@gmail.com>
2019-02-20 12:44:01 +0300

    Remove unuseful information

--- test.py
+++
@@ -1,7 +0,0 @@
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-...
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-
